a53b3f1e9f20490f37c78384825a7ca7ee65a734
Guillaume Malette <gmalette@gmail.com>
2014-03-28 23:03:25 -0400

    Headers

--- lib/pubsubstub.rb
+++ lib/pubsubstub.rb
@@ -41,6 +41,11 @@ module Pubsubstub
 
     get '/', provides: 'text/event-stream' do
       status(200)
+      header({
+        'Cache-Control' => 'no-cache',
+        'X-Accel-Buffering' => 'no',
+        'Connection' => 'keep-alive',
+      })
       stream(:keep_open) do |connection|
         @connections << connection
         channels = params[:channels] || [:default]
