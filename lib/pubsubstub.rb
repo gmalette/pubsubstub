7a2d2dab0d8fff8fdda22ac2e60f87170e1dce1c
Jean Boussier <jean.boussier@gmail.com>
2016-04-21 15:25:52 -0400

    Provide a shim for RedisPubSub.publish (deprecated)

--- lib/pubsubstub.rb
+++ lib/pubsubstub.rb
@@ -23,8 +23,8 @@ module Pubsubstub
     attr_accessor :heartbeat_frequency, :redis_url, :channels_scrollback_size,
       :channels_scrollback_ttl, :logger, :reconnect_timeout, :error_handler
 
-    def publish(channel_name, event)
-      Channel.new(channel_name).publish(event)
+    def publish(channel_name, *args)
+      Channel.new(channel_name).publish(Event.new(*args))
       nil
     end
 
@@ -69,4 +69,13 @@ module Pubsubstub
   self.channels_scrollback_size = 1000
   self.channels_scrollback_ttl = 24 * 60 * 60
   self.reconnect_timeout = 10_000
+
+  # Deprecated. Use Pubsubstub.publish instead
+  module RedisPubSub
+    extend self
+
+    def publish(channel_name, event)
+      Channel.new(channel_name).publish(event)
+    end
+  end
 end
