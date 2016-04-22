0d3b21e2171b89be008d8187b13633adc81684b4
Jean Boussier <jean.boussier@gmail.com>
2016-04-22 00:07:07 -0400

    Fix stupid typo in Logging#info

--- lib/pubsubstub/logging.rb
+++ lib/pubsubstub/logging.rb
@@ -1,7 +1,7 @@
 module Pubsubstub
   module Logging
     def info
-      Pubsubstub.logger.debug { "[#{self.class.name}] #{yield}" }
+      Pubsubstub.logger.info { "[#{self.class.name}] #{yield}" }
     end
 
     def debug
